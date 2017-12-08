db1fe62527d95667e86fd647fe5da88b0f09d4e4
Pontus Sjögren <Ext-PSjogren@luxoft.com>
2017-12-08 12:56:59 +0100

    Added day-05

---
+++ day-05/main.cpp
@@ -0,0 +1,61 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+#include <AOCUtils/io.h>
+
+uint32_t part_one(std::vector<int32_t> numbers)
+{
+    uint32_t steps = 0;
+
+    auto it = numbers.begin();
+    int32_t value = 0;
+
+    while(std::distance(it, numbers.end()) > value) {
+        std::advance(it, value);
+
+        value = *it;
+        (*it) = value + 1;
+
+        steps++;
+    }
+
+    return steps;
+}
+
+uint32_t part_two(std::vector<int32_t> numbers)
+{
+    uint32_t steps = 0;
+
+    auto it = numbers.begin();
+    int32_t value = 0;
+
+    while(std::distance(it, numbers.end()) > value) {
+        std::advance(it, value);
+
+        value = *it;
+
+        if(value >= 3) {
+            (*it) = value - 1;
+        }
+        else {
+            (*it) = value + 1;
+        }
+
+        steps++;
+    }
+
+    return steps;
+}
+
+int main()
+{
+    AOC::print_header("05");
+
+    auto numbers = AOC::convert_strings<int32_t>(AOC::read_lines("input.txt"));
+    //std::vector<int32_t> numbers { 0, 3, 0, 1, -3 };
+
+    std::cout << "Solution for part one: " << part_one(numbers) << std::endl;
+    std::cout << "Solution for part two: " << part_two(numbers) << std::endl;
+
+}
